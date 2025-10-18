05e0722434b7d09efd6bb384419a67f6ce6e3e6f
Payel Banerjee <payelbanerjee9319@gmail.com>
2025-10-18 18:30:56 +0530

    my first commit

---
+++ variable.js
@@ -0,0 +1,12 @@
+const accId = 123456; //we can't reassign it
+let accEmail = "a@gmail.com";
+var accphn = "4321567540";
+accCity = "kolkata";
+let accState;
+
+// accId = 4334; //can reassign only if let wriiten
+accEmail = "ap@gmail.com"; //can reassign only if let wriiten
+accphn = 433343453534; //can reassign only if let wriiten
+accCity = "abc"; //can reassign only if let wriiten
+
+console.log([accId, accEmail, accphn, accCity, accState]); //[123456, 'ap@gmail.com', 433343453534, 'abc', undefined]
